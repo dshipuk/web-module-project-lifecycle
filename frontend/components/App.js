4466b80236b2e7c9e05651f4d989b921297cc0aa
unknown <dshipuk@yahoo.com>
2022-04-12 15:00:25 -0700

    Fetched Todos From Api and Added them to dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+
+  }
+  
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          ...this.state,
+          todos: res.data.data
+        })
+      })
+      .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    // Fetch all todos from server
+    this.fetchAllTodos();
+  }
+
+
+
   render() {
-    return null
+    return(
+      <div>
+        <div id="error">Error: No error here</div>
+        <div id="todos">
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td => {
+              return <div key={td.id}>{td.name}</div>
+            })
+          }
+        </div>
+        <form id="todoForm">
+          <input type="text" placeholder="Type todo"></input>
+          <input type="submit"></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
