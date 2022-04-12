4b009b5913430cad4b9dc957890a518838d6d5bd
unknown <dshipuk@yahoo.com>
2022-04-12 16:08:11 -0700

    Can complete todos and show and hide todos on click of button

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
     todos: [],
     error: '',
     toDoNameInput: '',
+    displayCompleted: true
   }
   
   handleChanges = event => {
@@ -34,15 +35,29 @@ export default class App extends React.Component {
     ...this.state,
     toDoNameInput: ""
   })
-  
+
   setAxiosResponseError = err => this.setState({ ...this.state, error: err.response.data.message})
 
   postNewTodo = () => {
     axios.post(URL, { name: this.state.toDoNameInput })
       .then(res => {
         console.log(res)
-        this.fetchAllTodos()
+        
+        this.setState({ ...this.state, todos: this.state.todos.concat(res.data.data) })
+
         this.resetForm()
+      })
+      .catch(this.setAxiosResponseError)
+  }
+
+  toggleCompleted = id => event => {
+    axios.patch(`${URL}/${id}`)
+      .then(res => {
+        console.log(res)
+        this.setState({ ...this.state, todos: this.state.todos.map(td => {
+          if(td.id !== id) return td
+          return res.data.data
+        })})
       })
       .catch(this.setAxiosResponseError)
   }
@@ -57,6 +72,13 @@ export default class App extends React.Component {
     this.postNewTodo()
   }
 
+  clearCompleted = () => {
+    this.setState({
+      ...this.state,
+      displayCompleted: !this.state.displayCompleted
+    })
+  }
+
 
 
   render() {
@@ -66,16 +88,18 @@ export default class App extends React.Component {
         <div id="todos">
           <h2>Todos:</h2>
           {
-            this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
-            })
+            this.state.todos.reduce((acc, td) => {
+              if (this.state.displayCompleted || !td.completed) return acc.concat(<div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? "- Completed" : ""} </div>)
+              return acc
+              // return 
+            }, [])
           }
         </div>
         <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
           <input onChange={this.handleChanges} value={this.state.toDoNameInput} type="text" placeholder="Type todo"></input>
           <input type="submit"></input>
-          <button>Clear Completed</button>
         </form>
+        <button onClick={this.clearCompleted}>{this.state.displayCompleted ? "Hide" : "Show"} Completed</button>
       </div>
     )
   }
